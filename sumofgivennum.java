0b13c6357b298fd28b6f27d70c23d4a97c571366
Ragavimano20045 <127301974+Ragavimano20045@users.noreply.github.com>
2024-05-27 21:16:39 +0530

    Add files via upload

---
+++ sumofgivennum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class sumofgivennum
+ {
+    public static void main(String args[])
+{
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum=0;
+    for(int i=a;i<=b;i++)
+    sum+=i;
+    {
+        System.out.println(sum);
+    }
+    sc.close();
+
+}
+}
